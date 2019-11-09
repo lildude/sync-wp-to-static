3499b5cb39cb1639eae60e9017df6e5d184d333d
Colin Seymour <lildood@gmail.com>
2019-11-09 15:06:48 +0000

    Def class and  test

--- lib/sync_wp_to_static.rb
+++ lib/sync_wp_to_static.rb
@@ -1,2 +1,11 @@
 # frozen_string_literal: true
 
+require 'colorize'
+require 'date'
+require 'json'
+require 'octokit'
+require 'open-uri'
+
+# Class that syncs Wordpress posts to a static site's GitHub repo
+class SyncWpToStatic
+end
--- test/sync_wp_to_static_test.rb
+++ test/sync_wp_to_static_test.rb
@@ -17,3 +17,9 @@ require './lib/sync_wp_to_static'
 
 ENV['RACK_ENV'] ||= 'test'
 
+class SyncWpToStaticTest < Minitest::Test
+  def test_it_works
+    obj = SyncWpToStatic.new
+    assert obj
+  end
+end
